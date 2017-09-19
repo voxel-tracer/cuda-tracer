04f10692a5975fbc8d1ae2a8d235158ccf2276c2
voxel-tracer <voxel.tracer@gmail.com>
2017-09-19 07:15:45 -0700

    compiler optimized for speed

--- vs-tracer/raytracer.cu
+++ vs-tracer/raytracer.cu
@@ -1,22 +1,3 @@
-/**
- * Copyright 1993-2015 NVIDIA Corporation.  All rights reserved.
- *
- * Please refer to the NVIDIA end user license agreement (EULA) associated
- * with this source code for terms and conditions that govern your use of
- * this software. Any use, reproduction, disclosure, or distribution of
- * this software and related documentation outside the terms of the EULA
- * is strictly prohibited.
- *
- */
-
-/**
- * Vector addition: C = A + B.
- *
- * This sample is a very basic sample that implements element by element
- * vector addition. It is the same as the sample illustrating Chapter 2
- * of the programming guide with some additions like error checking.
- */
-
 #include <iostream>
 #include <float.h>
 #include <fstream>
